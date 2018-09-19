c6fcf5172cd26aedf061b16d78ae40017e6053cd
Vladim YSky <tuned@ngs.ru>
2018-09-19 21:53:12 +0700

    Add project files.

---
+++ YGX_Reader/Reader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Elements;
+
+namespace YGX_Reader
+{
+    public class Reader
+    {
+        private XElement xelYGX;
+
+        public Reader(string FileName) {
+            xelYGX = XElement.Load(FileName);
+        }
+    }
+}
